81d449e60eb7fc0c9a764f293c88b9094c10447d
jaimeandreshernandez <ja_hern@live.concordia.ca>
2017-09-16 16:37:39 -0400

    first

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../HTNapp.zip');
+var kuduApi = 'https://HTNapp.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$HTNapp';
+var password = 'p6Ww5zviTzbmdiBmdxDindLjnW5NKpt0H8dfoJ88Ad5yujLcQ626EatQ2FzN';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('HTNapp publish');
+  } else {
+    console.error('failed to publish HTNapp', err);
+  }
+});
